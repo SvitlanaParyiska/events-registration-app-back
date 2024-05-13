15078202880ccd512ec12f735a098a1f4c549ff3
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2024-05-13 16:32:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const app = require("./app");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const { DB_HOST, PORT } = process.env;
+const port = PORT || 6000;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful"),
-      app.listen(PORT, () => {
+      app.listen(port, () => {
         console.log(`Server running. Use our API on port: ${PORT}`);
       });
   })
